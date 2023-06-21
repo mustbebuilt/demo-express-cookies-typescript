66cc84a297faee73f32b36afabba554209969022
martin@mustbebuilt.co.uk <martinmustbebuilt@gmail.com>
2023-06-21 10:02:55 +0100

    tidy

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,14 +1,10 @@
 import express, { Router } from "express";
-import {
-  faveColourController,
-  faveColourPostController,
-  resetController
-} from "../controllers/controllers";
+import * as controller from "../controllers/controllers";
 
 const router: Router = express.Router();
 
-router.get("/", faveColourController);
-router.post("/", faveColourPostController);
-router.get("/reset", resetController);
+router.get("/", controller.faveColourController);
+router.post("/", controller.faveColourPostController);
+router.get("/reset", controller.resetController);
 
 export default router;
